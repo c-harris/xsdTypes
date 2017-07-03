b21afa677e4c9940aff309560ec388cd3430c5d6
c-harris <c.harris@communicationinnovations.com.au>
2017-07-03 16:24:58 +0000

    Apply fixes from StyleCI

--- src/xsIDREF.php
+++ src/xsIDREF.php
@@ -1,5 +1,6 @@
 <?php
 namespace AlgoWeb\xsdTypes;
+
 /**
  * CThe type xsd:IDREF is used for an attribute that references an ID. All attributes of type xsd:IDREF must reference
  * an xsd:ID in the same XML document. A common use case for xsd:IDREF is to create a cross-reference to a particular
