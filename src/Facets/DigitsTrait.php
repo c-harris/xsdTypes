299e3954c39fa62cd6a939d01bfe0163d6939ebc
c-harris <c.harris@communicationinnovations.com.au>
2017-07-03 22:18:51 +1000

    setup the digits trait

---
+++ src/Facets/DigitsTrait.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AlgoWeb\xsdTypes\Facets;
+
+
+trait digitsTrait
+{
+    /**
+     * @Exclude
+     * @var integer Specifies the maximum number of decimal places allowed. Must be equal to or greater than zero
+     */
+    private $fractionDigits = null;
+    /**
+     * @Exclude
+     * @var integer Specifies the exact number of digits allowed. Must be greater than zero
+     */
+    private $totalDigits = null;
+
+    public function setTotalDigits($fd)
+    {
+        $this->checkDigitLength($fd);
+        $this->totalDigits = $fd;
+    }
+
+    private function checkDigitLength($fd)
+    {
+        if (!is_numeric($fd)) {
+            throw new \InvalidArgumentException(
+                "the provided fractionDigits for  " . __CLASS__ . " is non numeric "
+            );
+        }
+        if (abs($fd) != $fd) {
+            throw new \InvalidArgumentException(
+                "the provided fractionDigits for  " . __CLASS__ . " must be non negative "
+            );
+        }
+        if (round($fd) != $fd) {
+            throw new \InvalidArgumentException(
+                "the provided fractionDigits for  " . __CLASS__ . " must be a whole number "
+            );
+        }
+    }
+
+    public function checkTotalDigits($v)
+    {
+        if (null == $this->totalDigits) {
+            return;
+        }
+        $stringVal = explode(".", (string)$v);
+        if ($this->totalDigits < strlen($stringVal[0])) {
+            throw new \InvalidArgumentException(
+                "the number of fractionDigits for  " . __CLASS__ . " is greater the allowed. "
+            );
+        }
+    }
+
+    public function setFractionDigits($fd)
+    {
+        $this->checkDigitLength($fd);
+        $this->fractionDigits = $fd;
+    }
+
+    public function checkFractionDigits($v)
+    {
+        if (null == $this->fractionDigits) {
+            return;
+        }
+        $stringVal = explode(".", (string)$v);
+        if (2 == count($stringVal)) {
+            if ($this->fractionDigits < strlen($stringVal[1])) {
+                throw new \InvalidArgumentException(
+                    "the number of fractionDigits for  " . __CLASS__ . " is greater the allowed. "
+                );
+            }
+        }
+    }
+
+    public function fixFractionDigits($v)
+    {
+        if (null != $this->fractionDigits) {
+            return round($v, $this->fractionDigits);
+        }
+    }
+}
