6fd109cbf8139da2cc2c0aec02a492369ccc7145
c-harris <c.harris@communicationinnovations.com.au>
2017-07-07 01:07:29 +1000

    added Language Tests

--- tests/unit/xsLanguageTest.php
+++ tests/unit/xsLanguageTest.php
@@ -9,9 +9,60 @@ namespace AlgoWeb\xsdTypes;
 class xsLanguageTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \AlgoWeb\xsdTypes\xsLanguage
+     * @dataProvider testxsLanguageValidDataProvider
      */
-    protected $object;
+    public function testxsLanguageValid($input, $message)
+    {
+        try {
+            $d = new xsLanguage($input);
+            $s = (string)$d;
+        } catch (\Exception $e) {
+            $this->fail($message . ' with Exception ' . $e->getMessage());
+        }
+    }
+
+    public function testxsLanguageValidDataProvider()
+    {
+        return array(
+            array('en', 'English'),
+            array('en-GB', 'UK English'),
+            array('en-US', 'US English'),
+            array('fr', 'French'),
+            array('de', 'German'),
+            array('es', 'Spanish'),
+            array('it', 'Italian'),
+            array('nl', 'Dutch'),
+            array('zh', 'Chinese'),
+            array('ja', 'Japanese'),
+            array('ko', 'Korean'),
+            array('i-navajo', 'IANA-registered language'),
+            array('x-Newspeak', 'private, unregistered language'),
+            array('any-value-with-short-parts', 'although a schema processor will consider this value valid, ' .
+                'it does not follow RFC 3066 guidelines'),
+        );
+    }
+
+    /**
+     * @dataProvider testxsLanguageInvalidDataProvider
+     */
+    public function testxsLanguageInvalid($input, $message)
+    {
+        try {
+            $d = new xsLanguage($input);
+            $s = (string)$d;
+            $this->fail($message);
+        } catch (\Exception $e) {
+        }
+        $this->assertEquals('', $s, $message);
+    }
+
+    public function testxsLanguageInvalidDataProvider()
+    {
+        return array(
+            array('longerThan8', 'parts may not exceed 8 characters in length'),
+            array('', 'an empty value is not valid, unless xsi:nil is used'),
+        );
+    }
 
     /**
      * Sets up the fixture, for example, opens a network connection.
@@ -20,7 +71,6 @@ class xsLanguageTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->object = new \AlgoWeb\xsdTypes\xsLanguage();
     }
 
     /**
@@ -30,17 +80,5 @@ class xsLanguageTest extends \PHPUnit_Framework_TestCase
     protected function tearDown()
     {
         parent::tearDown();
-    }
-
-    /**
-     * @covers \AlgoWeb\xsdTypes\xsLanguage::__toString
-     * @todo   Implement test__toString().
-     */
-    public function test__toString()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
     }
 }
