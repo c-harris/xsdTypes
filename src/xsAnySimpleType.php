e1e4db2e6e315bd220416464b2313430827eff54
c-harris <c.harris@communicationinnovations.com.au>
2017-07-06 17:49:12 +1000

    Apply fixes

--- src/xsAnySimpleType.php
+++ src/xsAnySimpleType.php
@@ -45,10 +45,10 @@ abstract class xsAnySimpleType
             }
         } catch (\Exception $e) {
             trigger_error($e->getMessage() .'--'.$e->getTraceAsString());
-            $this->value = "";
+            $this->value = '';
         }
-        if(is_array($this->value)){
-            return implode(" ", $this->value);
+        if (is_array($this->value)) {
+            return implode(' ', $this->value);
         }
         return (string)$this->value;
     }
