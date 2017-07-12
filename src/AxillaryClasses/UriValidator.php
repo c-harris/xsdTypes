cd8a754743cea8b2e68b6d26c7c4be50ac114869
c-harris <c.harris@communicationinnovations.com.au>
2017-07-12 12:09:21 +0000

    Apply fixes from StyleCI

--- src/AxillaryClasses/UriValidator.php
+++ src/AxillaryClasses/UriValidator.php
@@ -2,18 +2,17 @@
 
 namespace AlgoWeb\xsdTypes\AxillaryClasses;
 
-
 class UriValidator
 {
     const URI_REGEXP = '/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#?(.*))?/';
 
     /**
      * Validate a URI according to RFC 3986 (+errata)
-     * (See: http://www.rfc-editor.org/errata_search.php?rfc=3986)
+     * (See: http://www.rfc-editor.org/errata_search.php?rfc=3986).
      *
      * @param string $uri the URI to validate
      *
-     * @return boolean TRUE when the URI is valid, FALSE when invalid
+     * @return bool TRUE when the URI is valid, FALSE when invalid
      */
     public static function validate($uri)
     {
