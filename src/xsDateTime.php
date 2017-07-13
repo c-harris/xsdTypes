3ee287dc72d1972cd7a30a6205d1808b1272ad2d
c-harris <c.harris@communicationinnovations.com.au>
2017-07-13 08:17:27 +0000

    Apply fixes from StyleCI

--- src/xsDateTime.php
+++ src/xsDateTime.php
@@ -35,15 +35,15 @@ class xsDateTime extends xsAnySimpleType
     {
         parent::fixValue();
         $v = new \DateTime($this->value);
-        $this->value = $v->format("Y-m-d\TH:i:s") . $this->fixFractionOfSecond($v) . $v->format("P");
+        $this->value = $v->format("Y-m-d\TH:i:s") . $this->fixFractionOfSecond($v) . $v->format('P');
     }
 
     private function fixFractionOfSecond(\DateTime $v)
     {
-        if (0 != (int)$v->format("u")) {
-            return "." . $v->format("u") / 100000;
+        if (0 != (int)$v->format('u')) {
+            return '.' . $v->format('u') / 100000;
         }
-        return "";
+        return '';
     }
 
     protected function isOK()
