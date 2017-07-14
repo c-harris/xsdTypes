5a91d80bf5114a5d91ad4659de7b0aa275d97c27
c-harris <c.harris@communicationinnovations.com.au>
2017-07-14 17:01:04 +0000

    Apply fixes from StyleCI

--- src/AxillaryClasses/Calender.php
+++ src/AxillaryClasses/Calender.php
@@ -3,11 +3,9 @@
  * Created by PhpStorm.
  * User: Barnso
  * Date: 14/07/2017
- * Time: 8:28 PM
+ * Time: 8:28 PM.
  */
-
 namespace AlgoWeb\xsdTypes\AxillaryClasses;
-
 
 class Calender
 {
@@ -28,25 +26,25 @@ class Calender
     private function validateInput($year = null, $month = null, $day = null)
     {
         if (null === $year && null === $month && null === $day) {
-            throw new \InvalidArgumentException("A Caldender class must have at least a day, month, or year");
+            throw new \InvalidArgumentException('A Caldender class must have at least a day, month, or year');
         }
         if (null !== $year && null === $month && null !== $day) {
-            throw new \InvalidArgumentException("a year day value is not valid");
+            throw new \InvalidArgumentException('a year day value is not valid');
         }
     }
 
     private function valdidateState()
     {
         if (null !== $this->day && (1 > $this->day || 31 < $this->day)) {
-            throw new \InvalidArgumentException("the day must be greater 0 and less then 32");
+            throw new \InvalidArgumentException('the day must be greater 0 and less then 32');
         }
         if (null !== $this->month && (1 > $this->month || 12 < $this->month)) {
-            throw new \InvalidArgumentException("the month must be greater 0 and less then 12");
+            throw new \InvalidArgumentException('the month must be greater 0 and less then 12');
         }
         if ($this->month !== null && $this->day !== null &&
             cal_days_in_month($this->getYearOrHolder(), $this->month, $this->day) < $this->day
         ) {
-            throw new \InvalidArgumentException("there are not that many days in the assigned month");
+            throw new \InvalidArgumentException('there are not that many days in the assigned month');
         }
     }
 
@@ -68,7 +66,7 @@ class Calender
         $re = '/----(0[1-9]|1[0-9]|2[0-9]|3[0-1]|[1-9])([-+][0-1]\d:[0-6]\d|Z*)/';
         preg_match_all($re, $day, $matches, PREG_SET_ORDER, 0);
         if (count($matches) != 1 && count($matches[0]) != 3) {
-            throw new \InvalidArgumentException("Unable to extract day from input string");
+            throw new \InvalidArgumentException('Unable to extract day from input string');
         }
         return new self(null, null, $matches[0][1], $matches[0][2]);
     }
@@ -78,10 +76,9 @@ class Calender
         $re = '/--(1[0-2]|0[1-9]|[1-9])-(0[1-9]|1[0-9]|2[0-9]|3[0-1]|[1-9])([-+][0-1]\d:[0-6]\d|Z*)/';
         preg_match_all($re, $monthDay, $matches, PREG_SET_ORDER, 0);
         if (count($matches) != 1 && count($matches[0]) != 4) {
-            throw new \InvalidArgumentException("Unable to extract month day from input string");
+            throw new \InvalidArgumentException('Unable to extract month day from input string');
         }
         return new self(null, $matches[0][1], $matches[0][2], $matches[0][3]);
-
     }
 
     public static function fromMonth($month)
@@ -89,7 +86,7 @@ class Calender
         $re = '/--(1[0-2]|0[1-9]|[1-9])([-+][0-1]\d:[0-6]\d|Z*)/';
         preg_match_all($re, $month, $matches, PREG_SET_ORDER, 0);
         if (count($matches) != 1 && count($matches[0]) != 3) {
-            throw new \InvalidArgumentException("Unable to extract month from input string");
+            throw new \InvalidArgumentException('Unable to extract month from input string');
         }
         return new self(null, $matches[0][1], null, $matches[0][2]);
     }
@@ -99,7 +96,7 @@ class Calender
         $re = '/(\d{4})-(1[0-2]|0[1-9]|[1-9])([-+][0-1]\d:[0-6]\d|Z*)/';
         preg_match_all($re, $yearMonth, $matches, PREG_SET_ORDER, 0);
         if (count($matches) != 1 && count($matches[0]) != 3) {
-            throw new \InvalidArgumentException("Unable to extract month from input string");
+            throw new \InvalidArgumentException('Unable to extract month from input string');
         }
         return new self($matches[0][1], null, $matches[0][2], $matches[0][3]);
     }
@@ -109,7 +106,7 @@ class Calender
         $re = '/(\d{4})([-+][0-1]\d:[0-6]\d|Z*)/';
         preg_match_all($re, $year, $matches, PREG_SET_ORDER, 0);
         if (count($matches) != 1 && count($matches[0]) != 3) {
-            throw new \InvalidArgumentException("Unable to extract month from input string");
+            throw new \InvalidArgumentException('Unable to extract month from input string');
         }
         return new self($matches[0][1], null, null, $matches[0][2]);
     }
