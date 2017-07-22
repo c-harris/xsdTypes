1bab7509a5b08b100b95499f73848db3b630fe2d
c-harris <c.harris@communicationinnovations.com.au>
2017-07-22 19:36:30 +1000

    Allowed adding multiple values to an enumeration trait (#30)
    * Update EnumerationTrait.php

--- src/Facets/EnumerationTrait.php
+++ src/Facets/EnumerationTrait.php
@@ -25,8 +25,22 @@ trait EnumerationTrait
         if (!is_array($this->enumeration)) {
             $this->enumeration = [];
         }
-        $this->enumeration[] = $enumerationValue;
+        $this->handleAddArray($enumerationValue) ? :$this->enumeration[] = $enumerationValue;
     }
+    /**
+     * @param string $enumerationValue adds a value to the enumeration set
+     */
+    private function handleAddArray($enumerationValue)
+    {
+        if (!is_array($enumerationValue)) {
+            return false;
+        }
+        foreach ($enumerationValue as $eValue) {
+            $this->addEnumeration($eValue);
+        }
+        return;
+    }
+        
 
     private function checkEnumeration($v)
     {
