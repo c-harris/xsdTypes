0e8ca3a49ff7e4ce1ee59420254da8e443729324
c-harris <c.harris@communicationinnovations.com.au>
2017-07-22 08:00:59 +0000

    Apply fixes from StyleCI

--- src/Facets/EnumerationTrait.php
+++ src/Facets/EnumerationTrait.php
@@ -25,19 +25,19 @@ trait EnumerationTrait
         if (!is_array($this->enumeration)) {
             $this->enumeration = [];
         }
-        if($this->handleAddArray($enumerationValue)){
-
+        if ($this->handleAddArray($enumerationValue)) {
         }
         $this->handleAddArray($enumerationValue) ? :$this->enumeration[] = $enumerationValue;
     }
     /**
      * @param string $enumerationValue adds a value to the enumeration set
      */
-    private function handleAddArray($enumerationValue){
-        if(!is_array($enumerationValue)){
+    private function handleAddArray($enumerationValue)
+    {
+        if (!is_array($enumerationValue)) {
             return false;
         }
-        foreach($enumerationValue as $eValue){
+        foreach ($enumerationValue as $eValue) {
             $this->addEnumeration($eValue);
         }
         return;
