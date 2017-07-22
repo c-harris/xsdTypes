3f39ced78fd7d8934fb6007cb36b3ebd8e22307c
c-harris <c.harris@communicationinnovations.com.au>
2017-07-22 18:00:55 +1000

    Update EnumerationTrait.php

--- src/Facets/EnumerationTrait.php
+++ src/Facets/EnumerationTrait.php
@@ -25,8 +25,24 @@ trait EnumerationTrait
         if (!is_array($this->enumeration)) {
             $this->enumeration = [];
         }
-        $this->enumeration[] = $enumerationValue;
+        if($this->handleAddArray($enumerationValue)){
+
+        }
+        $this->handleAddArray($enumerationValue) ? :$this->enumeration[] = $enumerationValue;
     }
+    /**
+     * @param string $enumerationValue adds a value to the enumeration set
+     */
+    private function handleAddArray($enumerationValue){
+        if(!is_array($enumerationValue)){
+            return false;
+        }
+        foreach($enumerationValue as $eValue){
+            $this->addEnumeration($eValue);
+        }
+        return;
+    }
+        
 
     private function checkEnumeration($v)
     {
